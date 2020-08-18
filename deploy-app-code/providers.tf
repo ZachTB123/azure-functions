ad2a29c9e3bb45d37fa6535ff255c44a49b3c69b
ZachTB123 <becker.zach26@hotmail.com>
2020-08-17 22:07:47 -0500

    Create providers.tf

---
+++ deploy-app-code/providers.tf
@@ -0,0 +1,16 @@
+provider azurerm {
+  version = "2.23.0"
+  features {}
+}
+
+provider archive {
+  version = "1.3.0"
+}
+
+provider null {
+  version = "2.1.2"
+}
+
+provider random {
+  version = "2.2.1"
+}
